e4bc4ba7966e0fff7b7ce999bcc4256294ad5b98
amritesh-bhu <amriteshbhu438@gmail.com>
2025-07-16 12:57:04 +0530

    added the method and url logic to route it to the correct js function!!!

--- src/app.js
+++ src/app.js
@@ -1,8 +1,27 @@
 import http from 'http'
+import { writeContentToFileUsingCallback } from './routes/write-file.js'
 
-const httpServer = http.createServer()
 
+
+const httpServer = http.createServer((req, res) => {
+
+    const { method, url } = req
+    if (method === "GET" && url == '/writefile') {
+        // console.log("getting the request from postman client!!")
+        let body = ''
+        req.on('data', chunk => {
+            body += chunk.toString()
+        })
+        req.on('end', () => {
+            console.log("consoling buffer data", body)
+            const jsonData = JSON.parse(body)
+            const resData = writeContentToFileUsingCallback (jsonData)
+            res.end(JSON.stringify({ message: "Message received", resData }))
+        })
+    }
+
+})
 
 httpServer.listen(8080, () => {
     console.log('Server is listening on port 8080')
-})
+})  
