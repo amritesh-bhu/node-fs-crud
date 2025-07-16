f3d0ca0cbeee28400853f25f384a139d20be1977
amritesh-bhu <amriteshbhu438@gmail.com>
2025-07-16 18:19:08 +0530

    added the logic to add content in the file in form of json object!!!

--- src/routes/write-file.js
+++ src/routes/write-file.js
@@ -1,11 +1,16 @@
 import fs from "fs"
 
+const path = '/home/amritesh/workspace/filesystem-crud/files'
+
 // Writting a file using callback
 export const writeContentToFileUsingCallback = (jsonData) => {
 
     const { filename, content } = jsonData
+    console.log('content type ', content)
+
+    const jsonString = JSON.stringify(content)
     const writeFileAsync = (filename, callback) => {
-        fs.writeFile(`/home/amritesh/workspace/filesystem-crud/files/${filename}`, content, (err, data) => {
+        fs.writeFile(`${path}/${filename}`, jsonString, (err, data) => {
             if (err) callback(err, null)
             callback(null, data)
         })
@@ -16,4 +21,22 @@ export const writeContentToFileUsingCallback = (jsonData) => {
     })
 
     return 'content has been added to the file successfully!!'
+}
+
+export const writeContentToFileUsingPromise = async (jsonData) => {
+    const { filename, content } = jsonData
+
+    const jsonString = JSON.stringify(content)
+    const writeFileAsync = (filename, content) => {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(`${path}/${filename}`, jsonString, (err, data) => {
+                if (err) reject(err)
+                resolve(data)
+            })
+        })
+    }
+
+    const data = await writeFileAsync(filename, content)
+    console.log(data)
+    return "data added to the file!!!"
 }
