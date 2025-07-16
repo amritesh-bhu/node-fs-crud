77243b57e9e249214c789330c8f2d642a7ad3a8e
amritesh-bhu <amriteshbhu438@gmail.com>
2025-07-16 13:12:50 +0530

    added the url and method logic to create a file or overwrite content of the file using promise!!!

--- src/app.js
+++ src/app.js
@@ -15,11 +15,20 @@ const httpServer = http.createServer((req, res) => {
         req.on('end', () => {
             console.log("consoling buffer data", body)
             const jsonData = JSON.parse(body)
-            const resData = writeContentToFileUsingCallback (jsonData)
+            const resData = writeContentToFileUsingCallback(jsonData)
             res.end(JSON.stringify({ message: "Message received", resData }))
         })
+    } else if (method === "GET" && url == '/write-file-async') {
+        let body = ''
+        req.on('data', chunk => {
+            body += chunk.toString()
+        })
+        req.on('end', () => {
+            console.log("consoling the buffer data", body)
+            const jsonData = JSON.parse(body)
+            res.end(JSON.stringify({ message: "writting file using promise!!", data: jsonData }))
+        })
     }
-
 })
 
 httpServer.listen(8080, () => {
