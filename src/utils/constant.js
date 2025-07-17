21bf3f60d52eb14eafeae654f661e3f25d0bd3fc
amritesh-bhu <amriteshbhu438@gmail.com>
2025-07-18 01:04:41 +0530

    created this file to manage the filename and path to use everywhere!!!!

---
+++ src/utils/constant.js
@@ -0,0 +1,3 @@
+export const xyzFile = '/home/amritesh/workspace/filesystem-crud/files/xyz.txt'
+export const abcFile = '/home/amritesh/workspace/filesystem-crud/files/abc.txt'
+export const path = '/home/amritesh/workspace/filesystem-crud/files'
