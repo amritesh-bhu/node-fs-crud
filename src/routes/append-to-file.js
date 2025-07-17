a5a7da9470554c27276000fc3d81f42d7b50e600
amritesh-bhu <amriteshbhu438@gmail.com>
2025-07-18 00:38:53 +0530

    just added clarification comment!!!

---
+++ src/routes/append-to-file.js
@@ -0,0 +1,48 @@
+import fs from "fs"
+
+const fileName = '/home/amritesh/workspace/filesystem-crud/files/xyz.txt'
+
+
+// Writting a file using callback, This approach is not feasible to return something from here because 
+// the function return the value before the file operation is done because file operation is an asynchronous operation.
+
+export const appendingToFile = (jsonData) => {
+
+    const jsonString = JSON.stringify(jsonData)
+    const appendingToFileAsync = (callback) => {
+        fs.appendFile(fileName, `\n${jsonString}`, (err) => {
+            if (err) callback(err, null)
+            callback(null, "data appended to the file successfully!!")
+        })
+    }
+
+    appendingToFileAsync((err, message) => {
+        if (err) {
+            console.error(err)
+        }
+    })
+
+    return "data appended to the file successfully!!"
+}
+
+
+
+export const appendToFileUsingPromise = async (jsonData) => {
+    const jsonString = JSON.stringify(jsonData)
+    console.log(jsonString)
+    const appendToFileAsync = () => {
+        return new Promise((resolve, reject) => {
+            fs.appendFile(fileName, jsonString, (err) => {
+                if (err) reject(err)
+                resolve('appended content successfully!!!')
+            })
+        })
+    }
+
+    try {
+        const data = await appendToFileAsync()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+} 
